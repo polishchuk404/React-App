57c9cfb1a6ede771a14e31db4cbbe8c272371bf4
Vadim <polish4uck@gmail.com>
2020-03-27 14:56:50 +0200

    Updates

---
+++ precache-manifest.56fcd83337f9167cb2d0466323a39a17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b1f74536f93b6d6247c506187ca95f",
+    "url": "/github.io/React-App/index.html"
+  },
+  {
+    "revision": "23e86097a1605be510b6",
+    "url": "/github.io/React-App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bff0d0a8c81d9625cd5f",
+    "url": "/github.io/React-App/static/js/2.7fecf261.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/React-App/static/js/2.7fecf261.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23e86097a1605be510b6",
+    "url": "/github.io/React-App/static/js/main.6b801790.chunk.js"
+  },
+  {
+    "revision": "3d645b7db42d5c756f60",
+    "url": "/github.io/React-App/static/js/runtime-main.c051cf44.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/React-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
